931f672e78c455d3dabd542017caca42996bfc52
Matheus Chiarelli <63466884+matheuschiarelli11@users.noreply.github.com>
2024-06-30 15:23:15 -0300

    keep-alive check

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,16 @@
+const fetch = require('node-fetch');
+
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
     console.log(`${client.user.tag} is logged in and online`);
-  },
+
+    // Configurando o keep-alive
+    setInterval(() => {
+      fetch('https://discordbot-tjld.onrender.com')  
+        .then(res => res.text())
+        .then(text => console.log('Keep-alive:', text))
+        .catch(err => console.error('Erro ao manter o bot ativo:', err));
+    }, 5 * 60 * 1000); 
 };
